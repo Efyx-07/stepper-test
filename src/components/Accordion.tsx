457da3343aec5ef095e23b1fb3e03d95cf371ba1
Efyx-07 <fx.savary@orange.fr>
2024-11-03 01:38:26 +0100

    accordion

--- src/components/Accordion.tsx
+++ src/components/Accordion.tsx
@@ -6,26 +6,19 @@ import { useState } from 'react';
 import qas from '@/qa';
 
 export default function Accordion () {
-
-    // Utilise un état pour stocker l'index de l'élément actuellement ouvert
     const [openIndex, setOpenIndex] = useState<number | null>(null);
 
-    const toggleAnswer = (index: number): void => {
-        // Si l'index est déjà ouvert, on le ferme, sinon on l'ouvre
-        setOpenIndex(openIndex === index ? null : index);
-    };
-
+    const toggleAnswer = (index: number): void => setOpenIndex(openIndex === index ? null : index);
 
     return (
         <div className='accordion'>
             {qas.map((qa, index) => (
-                <div className="accordion-block" key={qa.id}>
+                <div className="accordion-block" key={qa.id} onClick={() => toggleAnswer(index)}>
                     <div className="qblock">
                         <p className="question">{qa.question}</p>
                         <Icon 
                             icon={openIndex === index ? "ep:arrow-up" : "ep:arrow-down"}  
                             className='arrow'
-                            onClick={() => toggleAnswer(index)}
                         />
                     </div>
                     <p className={`answer ${openIndex === index ? 'open' : ''}`}>
--- src/qa.ts
+++ src/qa.ts
@@ -6,7 +6,8 @@ const qas = [
                 Un permis de construire ou une déclaration préalable de travaux est nécessaire pour de nombreux travaux venant modifier l’aspect extérieur de votre habitat ou créant de la surface.
                 Il est bien connu de demander une autorisation pour construire une extension mais beaucoup moins lorsqu’il s’agit d’un abri de jardin ou l’installation d’une clôture.
                 Et pourtant, que ce soit pour les petits ou les grands projets, déposer un dossier de permis de construire ou de déclaration préalable de travaux est obligatoire.
-                Les principaux projets nécessitant une autorisation d’urbanisme sont les maisons, les agrandissements, les garages et carports, les abris de jardin, les annexes, les clôtures et portails, les piscines, les terrasses, les panneaux solaires, les modifications de menuiseries, les modifications de façades et de toitures.`
+                Les principaux projets nécessitant une autorisation d’urbanisme sont les maisons, les agrandissements, les garages et carports, les abris de jardin, les annexes, les clôtures et portails, les piscines, les terrasses, les panneaux solaires, les modifications de menuiseries, les modifications de façades et de toitures.
+                `
     },
 
     {
@@ -20,7 +21,22 @@ const qas = [
                 • Un projet qui reste opposable par un tiers
                 • Des assurances ne vous couvriront pas en cas de sinistres
 
-                Il ne faut donc pas prendre ces procédures légales à la légère et faire les choses dans les règles.`
+                Il ne faut donc pas prendre ces procédures légales à la légère et faire les choses dans les règles.
+                `
+    },
+
+    {
+        id: 3,
+        question: "Quelle est la différence entre un permis de construire et une déclaration préalable de travaux ?",
+        answer: `
+                On dit généralement qu'un permis de construire est demandé pour les gros travaux et la déclaration préalable de travaux pour les petits travaux. Mais c’est peu plus complexe puisque beaucoup de critères entrent en jeu.
+                Concernant les projets sans création de surface (comme les modifications de menuiseries, façades, installations de clôture…), il faut demander une déclaration préalable de travaux.
+
+                Sachez que ces deux types d’autorisation d'urbanisme sont quasiment identiques. Ce sont les mêmes règles d’urbanisme à étudier, les mêmes plans à réaliser et la même procédure à suivre.
+                Ils représentent la même charge de travail et la même complexité !
+
+                La seule différence se trouve dans les délais d'instruction de la mairie, généralement un peu plus courts pour une déclaration préalable de travaux que pour un permis de construire (à condition de ne pas être en zone protégée).
+                `
     },
 ];
 
